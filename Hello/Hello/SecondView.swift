47c7d15da9992a45f02d09ce83d3f8a1ed39d83b
young-ho <yhkim@yhkimui-MacBook-Air.local>
2017-04-28 22:45:34 +0900

    segue test

--- Hello/Hello/SecondView.swift
+++ Hello/Hello/SecondView.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondView.swift
 //  Hello
 //
 //  Created by yhkim on 2017. 4. 28..
@@ -8,22 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    @IBOutlet weak var label1: UILabel!
+class SecondView: UIViewController
+{
+    @IBAction func myunwindAction(unwindSeg:UIStoryboardSegue){}
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func touchButton(_ sender: Any) {
-        label1.text = "Good touch"
-    }
-
+    
 }
-
